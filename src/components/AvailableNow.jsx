ff4f7fb9564b3834d83b3525737ba3587c2ae455
Thilak <murgamthilak@gmail.com>
2025-06-29 08:30:29 +0530

    available now file

--- src/components/AvailableNow.jsx
+++ src/components/AvailableNow.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./AvailableNow.css";
+
 
 const AvailableNow = () => {
   const [bikes, setBikes] = useState([]);
